9d2cf9374190f5430b0fa88e8753ddec6ec25a61
Oscar Salas <salascorp@gmail.com>
2022-06-18 23:13:27 -0500

    decordaores fina

--- 7_decoradores.py
+++ 7_decoradores.py
@@ -26,13 +26,39 @@
 
 # saludo()
 
-def mayusculas(func):
-    def envoltura(texto):
-        return func(texto).upper()
-    return envoltura
+# def mayusculas(func):
+#     def envoltura(texto):
+#         return func(texto).upper()
+#     return envoltura
 
-@mayusculas
-def mensaje(nombre):
-    return f'{nombre}, recibiste un mensaje'
+# @mayusculas
+# def mensaje(nombre):
+#     return f'{nombre}, recibiste un mensaje'
 
-print(mensaje('cesar'))
+# print(mensaje('cesar'))
+
+from datetime import datetime
+import pyrsistent
+from pyrsistent import b
+
+def execution_time(func):
+    def wrapper(*args,**kwargs):
+        initial_time=datetime.now()
+        func(*args,**kwargs)
+        final_time=datetime.now()
+        time_elapsed=final_time-initial_time
+        print("pasaron "+ str(time_elapsed.total_seconds()) +" segundos")
+    return wrapper
+
+@execution_time
+def random_func():
+    for _ in range(1,1000000000):
+        pass
+
+#random_func()
+
+@execution_time
+def suma(a: int, b:int) -> int:
+    return a+b
+
+suma(5,5)
